9c35f4816c45d6abeb23f4dcf3d59516cf1508fa
Preecha Sattabut <precha.sattabut@gmail.com>
2020-05-17 02:08:02 +0700

    Charge socket url

--- src/pages/video/socket.js
+++ src/pages/video/socket.js
@@ -1,6 +1,7 @@
 import io from "socket.io-client"
 
-const socket = io("https://fathomless-mesa-78979.herokuapp.com", {
+// const socket = io("https://fathomless-mesa-78979.herokuapp.com", {
+const socket = io("https://localhost:8081", {
   path: "/bridge",
 })
 
