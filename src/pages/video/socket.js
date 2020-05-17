e3ec4d0317afc350c786861c39304ec0ee240fa0
Preecha Sattabut <precha.sattabut@gmail.com>
2020-05-18 01:32:01 +0700

    Fix

--- src/pages/video/socket.js
+++ src/pages/video/socket.js
@@ -1,7 +1,7 @@
 import io from "socket.io-client"
 
 // const socket = io("https://fathomless-mesa-78979.herokuapp.com", {
-const socket = io("https://203.154.83.208/api", {
+const socket = io("http://203.154.83.208/api", {
   path: "/bridge",
 })
 
